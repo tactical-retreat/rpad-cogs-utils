2b200f3d7d47cc51f3157270a6241200bfa68f68
nachoapps <nachoappsinc@gmail.com>
2017-04-23 00:50:44 -0400

    Fixing typo in update script

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -4,7 +4,7 @@
 # uploads them to GCS, setting them public.
 
 # Full pictures
-python3 /home/tactical0retreatrpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/full
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/full
 gsutil -m rsync -r image_data/full/corrected_data gs://rpad-discord.appspot.com/pad/full/
 gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/full/*
 
