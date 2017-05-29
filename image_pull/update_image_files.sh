b0fe6c7dc2cf179d3913e0a63da091240e3e5439
nachoapps <nachoappsinc@gmail.com>
2017-05-29 19:24:23 -0400

    Updating image pull script to account for NA/JP.

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -4,11 +4,16 @@
 # uploads them to GCS, setting them public.
 
 # Full pictures
-python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/full
-gsutil -m rsync -r image_data/full/corrected_data gs://rpad-discord.appspot.com/pad/full/
-gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/full/*
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/na/full --server=NA
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/jp/full --server=JP
+gsutil -m rsync -r image_data/na/full/corrected_data gs://rpad-discord.appspot.com/pad/na/full/
+gsutil -m rsync -r image_data/jp/full/corrected_data gs://rpad-discord.appspot.com/pad/jp/full/
+gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/na/full/*
+gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/jp/full/*
 
 # Portraits
-python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/portrait
-gsutil -m rsync -r image_data/portrait/corrected_data gs://rpad-discord.appspot.com/pad/portrait/
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/na/portrait --server=NA
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/jp/portrait --server=JP
+gsutil -m rsync -r image_data/portrait/na/corrected_data gs://rpad-discord.appspot.com/pad/na/portrait/
+gsutil -m rsync -r image_data/portrait/jp/corrected_data gs://rpad-discord.appspot.com/pad/jp/portrait/
 gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/portrait/*
