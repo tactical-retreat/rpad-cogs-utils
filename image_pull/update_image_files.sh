bd4b2b1e65573adfedd1ad1e88772937b8a70273
nachoapps <nachoappsinc@gmail.com>
2017-05-29 19:34:19 -0400

    Updating script to point to new mirubot cloud location.

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -6,14 +6,15 @@
 # Full pictures
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/na/full --server=NA
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/jp/full --server=JP
-gsutil -m rsync -r image_data/na/full/corrected_data gs://rpad-discord.appspot.com/pad/na/full/
-gsutil -m rsync -r image_data/jp/full/corrected_data gs://rpad-discord.appspot.com/pad/jp/full/
-gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/na/full/*
-gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/jp/full/*
+gsutil -m rsync -r image_data/na/full/corrected_data gs://mirubot/padimages/na/full/
+gsutil -m rsync -r image_data/jp/full/corrected_data gs://mirubot/padimages/jp/full/
+gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/full/*
+gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/full/*
 
 # Portraits
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/na/portrait --server=NA
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/jp/portrait --server=JP
-gsutil -m rsync -r image_data/portrait/na/corrected_data gs://rpad-discord.appspot.com/pad/na/portrait/
-gsutil -m rsync -r image_data/portrait/jp/corrected_data gs://rpad-discord.appspot.com/pad/jp/portrait/
-gsutil -m acl ch -u AllUsers:R gs://rpad-discord.appspot.com/pad/portrait/*
+gsutil -m rsync -r image_data/portrait/na/corrected_data gs://mirubot/padimages/na/portrait/
+gsutil -m rsync -r image_data/portrait/jp/corrected_data gs://mirubot/padimages/jp/portrait/
+gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/portrait/*
+gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/portrait/*
