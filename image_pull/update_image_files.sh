b26f94fe1f0a84835f39d0b61dd3e407894aa2db
nachoapps <nachoappsinc@gmail.com>
2017-10-21 19:16:56 -0400

    Fix source directories for portrait upload.

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -14,7 +14,7 @@ gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/full/*
 # Portraits
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/na/portrait --server=NA
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/jp/portrait --server=JP
-gsutil -m rsync -r image_data/portrait/na/corrected_data gs://mirubot/padimages/na/portrait/
-gsutil -m rsync -r image_data/portrait/jp/corrected_data gs://mirubot/padimages/jp/portrait/
+gsutil -m rsync -r image_data/na/portrait/corrected_data gs://mirubot/padimages/na/portrait/
+gsutil -m rsync -r image_data/jp/portrait/corrected_data gs://mirubot/padimages/jp/portrait/
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/portrait/*
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/portrait/*
