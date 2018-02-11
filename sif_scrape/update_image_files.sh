28ddce4ffe7f61460b1735e1ce2a9770c1d61a56
nachoapps <nachoappsinc@gmail.com>
2018-02-11 18:19:10 -0500

    Fix rsync source folder

--- sif_scrape/update_image_files.sh
+++ sif_scrape/update_image_files.sh
@@ -3,5 +3,5 @@
 # Downloads SIF images.
 
 python3 /home/tactical0retreat/rpad-cogs-utils/sif_scrape/image_download.py --output_dir=/home/tactical0retreat/sif_image_data/
-gsutil -m rsync -r sif_image_data/processed gs://mirubot/sifimages/processed
+gsutil -m rsync -r /home/tactical0retreat/sif_image_data/processed gs://mirubot/sifimages/processed
 gsutil -m acl ch -u AllUsers:R gs://mirubot/sifimages/processed/*
