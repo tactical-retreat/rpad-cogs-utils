054f2580ae3456ab4ac63e7fbea24e1c64dea8aa
nachoapps <nachoappsinc@gmail.com>
2018-03-04 13:36:11 -0500

    Listing full paths out

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -6,8 +6,8 @@
 # Full pictures
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/na/full --server=NA
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/jp/full --server=JP
-gsutil -m rsync -r image_data/na/full/corrected_data gs://mirubot/padimages/na/full/
-gsutil -m rsync -r image_data/jp/full/corrected_data gs://mirubot/padimages/jp/full/
+gsutil -m rsync -r /home/tactical0retreat/image_data/na/full/corrected_data gs://mirubot/padimages/na/full/
+gsutil -m rsync -r /home/tactical0retreat/image_data/jp/full/corrected_data gs://mirubot/padimages/jp/full/
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/full/*
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/full/*
 
@@ -31,7 +31,7 @@ python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsCombiner.p
   --jp_dir=/home/tactical0retreat/image_data/jp/portrait/local \
   --output_dir=/home/tactical0retreat/image_data/na/portrait/local
 
-gsutil -m rsync -r image_data/na/portrait/local gs://mirubot/padimages/na/portrait/
-gsutil -m rsync -r image_data/jp/portrait/local gs://mirubot/padimages/jp/portrait/
+gsutil -m rsync -r /home/tactical0retreat/image_data/na/portrait/local gs://mirubot/padimages/na/portrait/
+gsutil -m rsync -r /home/tactical0retreat/image_data/jp/portrait/local gs://mirubot/padimages/jp/portrait/
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/portrait/*
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/portrait/*
