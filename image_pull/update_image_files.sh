7da39346c604aba0caf72ee8d4b5bdab40375afc
nachoapps <nachoappsinc@gmail.com>
2018-03-04 11:23:49 -0500

    Adjusting to use the local portrait generation process

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -12,9 +12,21 @@ gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/full/*
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/full/*
 
 # Portraits
-python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/na/portrait --server=NA
-python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsDownload.py --output_dir=/home/tactical0retreat/image_data/jp/portrait --server=JP
-gsutil -m rsync -r image_data/na/portrait/corrected_data gs://mirubot/padimages/na/portrait/
-gsutil -m rsync -r image_data/jp/portrait/corrected_data gs://mirubot/padimages/jp/portrait/
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitGenerator.py \
+  --input_dir=/home/tactical0retreat/image_data/na/full/extract_data \
+  --card_types_file=/home/tactical0retreat/hosted_services/bots/Red-DiscordBot-PrivateMiru/data/padguide2/card_data.csv \
+  --card_templates_file=/home/tactical0retreat/rpad-cogs-utils/image_pull/wide_cards.png \
+  --server=na \
+  --output_dir=/home/tactical0retreat/image_data/na/portrait/local
+
+python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitGenerator.py \
+  --input_dir=/home/tactical0retreat/image_data/jp/full/extract_data \
+  --card_types_file=/home/tactical0retreat/hosted_services/bots/Red-DiscordBot-PrivateMiru/data/padguide2/card_data.csv \
+  --card_templates_file=/home/tactical0retreat/rpad-cogs-utils/image_pull/wide_cards.png \
+  --server=jp \
+  --output_dir=/home/tactical0retreat/image_data/jp/portrait/local
+      
+gsutil -m rsync -r image_data/na/portrait/local gs://mirubot/padimages/na/portrait/
+gsutil -m rsync -r image_data/jp/portrait/local gs://mirubot/padimages/jp/portrait/
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/portrait/*
 gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/portrait/*
