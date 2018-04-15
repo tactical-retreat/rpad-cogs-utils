9be9bef571616d0a5515b63a58d5e839fb089ebe
nachoapps <nachoappsinc@gmail.com>
2018-04-15 21:11:50 +0000

    fixing line endings, removing gsutil acl changes since bucket is public

--- image_pull/update_image_files.sh
+++ image_pull/update_image_files.sh
@@ -8,8 +8,6 @@ python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py 
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADTextureDownload.py --output_dir=/home/tactical0retreat/image_data/jp/full --server=JP
 gsutil -m rsync -r /home/tactical0retreat/image_data/na/full/corrected_data gs://mirubot/padimages/na/full/
 gsutil -m rsync -r /home/tactical0retreat/image_data/jp/full/corrected_data gs://mirubot/padimages/jp/full/
-gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/full/*
-gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/full/*
 
 # Portraits
 python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsGenerator.py \
@@ -33,5 +31,3 @@ python3 /home/tactical0retreat/rpad-cogs-utils/image_pull/PADPortraitsCombiner.p
 
 gsutil -m rsync -r /home/tactical0retreat/image_data/na/portrait/local gs://mirubot/padimages/na/portrait/
 gsutil -m rsync -r /home/tactical0retreat/image_data/jp/portrait/local gs://mirubot/padimages/jp/portrait/
-gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/na/portrait/*
-gsutil -m acl ch -u AllUsers:R gs://mirubot/padimages/jp/portrait/*
--- sif_scrape/update_image_files.sh
+++ sif_scrape/update_image_files.sh
@@ -4,4 +4,3 @@
 
 python3 /home/tactical0retreat/rpad-cogs-utils/sif_scrape/image_download.py --output_dir=/home/tactical0retreat/sif_image_data/
 gsutil -m rsync -r /home/tactical0retreat/sif_image_data/processed gs://mirubot/sifimages/processed
-gsutil -m acl ch -u AllUsers:R gs://mirubot/sifimages/processed/*
