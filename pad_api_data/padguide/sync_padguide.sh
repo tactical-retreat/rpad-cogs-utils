135ac985898d45a0ac9c0df773236e8ec2117072
nachoapps <nachoappsinc@gmail.com>
2018-06-17 10:38:27 -0400

    Fix download script path

--- pad_api_data/padguide/sync_padguide.sh
+++ pad_api_data/padguide/sync_padguide.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Downloading"
-python3 /home/tactical0retreat/rpad-cogs-utils/pad_api_data/download_files.py \
+python3 /home/tactical0retreat/rpad-cogs-utils/pad_api_data/padguide/download_files.py \
   --output_dir=/home/tactical0retreat/pad_data/padguide
 
 echo "Syncing"
