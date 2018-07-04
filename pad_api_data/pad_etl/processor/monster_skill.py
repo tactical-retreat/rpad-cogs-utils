bfc45c398ead96d87ebefaf1a7bea5307ab6a1b2
nachoapps <nachoappsinc@gmail.com>
2018-07-03 21:28:48 -0400

    Fix monster skill item, totally borked before

--- pad_api_data/pad_etl/processor/monster_skill.py
+++ pad_api_data/pad_etl/processor/monster_skill.py
@@ -1,39 +1,70 @@
-class MonsterItem(object):
-    def __init__(self, merged_card_jp: MergedCard, merged_card_na: MergedCard):
-        # Primary key, set later
-        self.ts_seq = None
-         
+# from datetime import date, datetime, timedelta
+import time
+#
+# from enum import Enum
+
+from . import db_util
+# from . import processor_util
+from ..data.skill import MonsterSkill
+from .merged_data import MergedCard
+
+
+def get_monster_skill_ids(mc: MergedCard):
+    args = {'monster_no': mc.card.card_id}
+    sql = "SELECT ts_seq_leader, ts_seq_skill FROM monster_list WHERE monster_no = {monster_no}"
+    return sql.format(**db_util.object_to_sql_params(args))
+
+
+def get_update_monster_skill_ids(mc: MergedCard, ts_seq_leader: int, ts_seq_skill: int):
+    args = {
+        'ts_seq_leader': ts_seq_leader,
+        'ts_seq_skill': ts_seq_skill,
+        'monster_no': mc.card.card_id
+    }
+    sql = """
+    UPDATE monster_list 
+    SET ts_seq_leader = {ts_seq_leader}, ts_seq_skill = {ts_seq_skill} 
+    WHERE monster_no = {monster_no}
+    """
+    return sql.format(**db_util.object_to_sql_params(args))
+
+
+class MonsterSkillItem(object):
+    def __init__(self, ts_seq: int, skill_jp: MonsterSkill, skill_na: MonsterSkill):
+        # Primary key
+        self.ts_seq = ts_seq
+
         # Not right but not useful either
-        self.mag_atk = 0  
+        self.mag_atk = 0
         self.mag_hp = 0
         self.mag_rcv = 0
-        self.reduce_dmg = 
-        
+        self.reduce_dmg = 0
+
         # Not sure what this order is used for
         self.order_idx = 0
-        
+
         # Used but not sure what for
         self.t_condition = 0
 
         # Seems (mostly) unused
-        self.rta_seq_1 = None 
+        self.rta_seq_1 = None
         self.rta_seq_2 = None
         self.ta_seq_1 = 0
         self.ta_seq_2 = 0
         self.tt_seq_1 = 0
         self.tt_seq_2 = 0
-        
-        # Useful fields        
+
+        # Useful fields
         self.tstamp = int(time.time())
-        self.ts_desc_jp = merged_card_jp.card.clean_description 
-        self.ts_desc_kr = 'unknown_{}'.format(merged_card_jp.card.skill_id)
-        self.ts_desc_us = merged_card_na.card.clean_description
-        self.ts_name_jp = merged_card_jp.card.name
-        self.ts_name_kr = 'unknown_{}'.format(merged_card_jp.card.skill_id)
-        self.ts_name_us = merged_card_na.card.name
-        self.turn_max = merged_card_jp.card.turn_max
-        self.turn_min = merged_card_jp.card.turn_min
-        
+        self.ts_desc_jp = skill_jp.clean_description
+        self.ts_desc_kr = 'unknown_{}'.format(skill_jp.skill_id)
+        self.ts_desc_us = skill_na.clean_description
+        self.ts_name_jp = skill_jp.name
+        self.ts_name_kr = 'unknown_{}'.format(skill_jp.skill_id)
+        self.ts_name_us = skill_na.name
+        self.turn_max = skill_jp.turn_max or 0
+        self.turn_min = skill_jp.turn_min or 0
+
         self.search_data = '{} {}'.format(self.ts_name_jp, self.ts_name_us)
 
     def insert_sql(self):
@@ -87,3 +118,7 @@ class MonsterItem(object):
             {turn_min},
             {t_condition});
         """
+        return sql.format(**db_util.object_to_sql_params(self))
+
+    def __repr__(self):
+        return 'MonsterSkillItem({} - {})'.format(self.ts_seq, self.search_data)
