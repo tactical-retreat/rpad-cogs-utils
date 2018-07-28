ba21a7f379a95e93557c9864bdf0e10b66028415
nachoapps <nachoappsinc@gmail.com>
2018-07-28 21:08:56 +0000

    fix grammar in skill

--- pad_api_data/pad_etl/processor/skill_info.py
+++ pad_api_data/pad_etl/processor/skill_info.py
@@ -1963,7 +1963,7 @@ def orb_remain_convert(arguments):
         _, c = convert_with_defaults('orb_remain',
                                      arguments,
                                      orb_remain_backups)(x)
-        c['skill_text'] += fmt_mult(c['atk_multiplier']) + 'x ATK when there is less than ' +\
+        c['skill_text'] += fmt_mult(c['atk_multiplier']) + 'x ATK when there are less than ' +\
             str(c['orb_count']) + ' orbs remaining'
         if c['bonus_atk_multiplier'] != 0:
             c['skill_text'] += ' up to ' + fmt_mult(c['atk_multiplier'] +
