34c3d2988c2b8acb08b77ad9c66d8e69d72d78bd
nachoapps <nachoappsinc@gmail.com>
2018-08-01 01:16:44 +0000

    bunch of bugfixes for image copier

--- pad_api_data/copy_image_data.py
+++ pad_api_data/copy_image_data.py
@@ -18,30 +18,39 @@ def parse_args():
     outputGroup.add_argument("--output_dir", required=True,
                              help="Dir to write padguide-formatted images to")
 
+    return parser.parse_args()
+
+def do_copy(src_dir, src_file, dest_dir, dest_file):
+    src_path = os.path.join(src_dir, src_file)
+    dest_path = os.path.join(dest_dir, dest_file)
+    if os.path.exists(src_path) and not os.path.exists(dest_file):
+        shutil.copy2(src_path, dest_path)
+
 
 def copy_images(args):
     base_dir = args.base_dir
     output_dir = args.output_dir
 
-    jp_icon_input_dir = os.path.join(base_dir, 'jp', 'portraits', 'local')
-    na_icon_input_dir = os.path.join(base_dir, 'na', 'portraits', 'local')
+    jp_icon_input_dir = os.path.join(base_dir, 'jp', 'portrait', 'local')
+    na_icon_input_dir = os.path.join(base_dir, 'na', 'portrait', 'local')
 
     jp_portrait_input_dir = os.path.join(base_dir, 'jp', 'full', 'corrected_data')
     na_portrait_input_dir = os.path.join(base_dir, 'na', 'full', 'corrected_data')
 
     for jp_id in range(1, 6000):
         monster_no = monster_id_mapping.jp_id_to_monster_no(jp_id)
-        shutil.copy2(os.path.join(jp_icon_input_dir, '{}.png'.format(jp_id)),
-                     os.path.join(output_dir, 'icon_{}.png'.format(monster_no)))
-        shutil.copy2(os.path.join(jp_portrait_input_dir, '{}.png'.format(jp_id)),
-                     os.path.join(output_dir, 'portrait_{}.png'.format(monster_no)))
+        monster_no_filled = str(monster_no).zfill(4)
+        do_copy(jp_icon_input_dir, '{}.png'.format(jp_id),
+                output_dir, 'icon_{}.png'.format(monster_no_filled))
+        do_copy(jp_portrait_input_dir, '{}.png'.format(jp_id),
+                output_dir, 'portrait_{}.png'.format(monster_no_filled))
 
     for na_id in monster_id_mapping.NA_VOLTRON_IDS:
         monster_no = monster_id_mapping.na_id_to_monster_no(na_id)
-        shutil.copy2(os.path.join(na_icon_input_dir, '{}.png'.format(na_id)),
-                     os.path.join(output_dir, 'icon_{}.png'.format(monster_no)))
-        shutil.copy2(os.path.join(na_portrait_input_dir, '{}.png'.format(na_id)),
-                     os.path.join(output_dir, 'portrait_{}.png'.format(monster_no)))
+        do_copy(na_icon_input_dir, '{}.png'.format(na_id),
+                output_dir, 'icon_{}.png'.format(monster_no_filled))
+        do_copy(na_portrait_input_dir, '{}.png'.format(na_id),
+                output_dir, 'portrait_{}.png'.format(monster_no_filled))
 
 
 if __name__ == '__main__':
