055e9143ab09087fd8aa8fad4def74618aa6ea8a
nachoapps <nachoappsinc@gmail.com>
2018-08-06 18:25:27 -0400

    bugfix for some tables not updating timestamp properly

--- pad_api_data/padguide_processor.py
+++ pad_api_data/padguide_processor.py
@@ -416,7 +416,7 @@ def database_diff_cards(db_wrapper, jp_database, na_database):
 
 
 def database_update_timestamps(db_wrapper):
-    get_tables_sql = 'SELECT `table` FROM get_timestamp'
+    get_tables_sql = 'SELECT `internal_table` FROM get_timestamp'
     tables = list(map(lambda x: x['table'], db_wrapper.fetch_data(get_tables_sql)))
     for table in tables:
         get_tstamp_sql = 'SELECT MAX(tstamp) as tstamp FROM `{}`'.format(table.lower() + '_list')
