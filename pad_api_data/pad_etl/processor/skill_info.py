1283e09ab27ed291c55f064ecba4b29bbf1f02e3
dchang0413@gmail.com <dchang0413@gmail.com>
2018-08-10 18:27:05 -0500

    Fixed suicide_random_nuke %HP text

--- pad_api_data/pad_etl/processor/skill_info.py
+++ pad_api_data/pad_etl/processor/skill_info.py
@@ -9,7 +9,71 @@ import sys
 
 from defaultlist import defaultlist
 
-from ..common.padguide_values import AWAKENING_MAP
+#from ..common.padguide_values import AWAKENING_MAP
+AWAKENING_MAP = {
+    0: '',  # No need.
+    1: 'Enhanced HP',
+    2: 'Enhanced Attack',
+    3: 'Enhanced Heal',
+    4: 'Reduce Fire Damage',
+    5: 'Reduce Water Damage',
+    6: 'Reduce Wood Damage',
+    7: 'Reduce Light Damage',
+    8: 'Reduce Dark Damage',
+    9: 'Auto-Recover',
+    10: 'Resistance-Bind',
+    11: 'Resistance-Dark',
+    12: 'Resistance-Jammers',
+    13: 'Resistance-Poison',
+    14: 'Enhanced Fire Orbs',
+    15: 'Enhanced Water Orbs',
+    16: 'Enhanced Wood Orbs',
+    17: 'Enhanced Light Orbs',
+    18: 'Enhanced Dark Orbs',
+    19: 'Extend Time',
+    20: 'Recover Bind',
+    21: 'Skill Boost',
+    22: 'Enhanced Fire Att.',
+    23: 'Enhanced Water Att.',
+    24: 'Enhanced Wood Att.',
+    25: 'Enhanced Light Att.',
+    26: 'Enhanced Dark Att.',
+    27: 'Two-Pronged Attack',
+    28: 'Resistance-Skill Bind',
+    29: 'Enhanced Heal Orbs',
+    30: 'Multi Boost',
+    31: 'Dragon Killer',
+    32: 'God Killer',
+    33: 'Devil Killer',
+    34: 'Machine Killer',
+    35: 'Balanced Killer',
+    36: 'Attacker Killer',
+    37: 'Physical Killer',
+    38: 'Healer Killer',
+    39: 'Evolve Material Killer',
+    40: 'Awaken Material Killer',
+    41: 'Enhance Material Killer',
+    42: 'Vendor Material Killer',
+    43: 'Enhanced Combo',
+    44: 'Guard Break',
+    45: 'Additional Attack',
+    46: 'Enhanced Team HP',
+    47: 'Enhanced Team RCV',
+    48: 'Damage Void Shield Penetration',
+    49: 'Awoken Assist',
+    50: 'Super Additional Attack',
+    51: 'Skill Charge',
+    52: 'Resistance-Bind＋',
+    54: 'Resistance-Cloud',
+    53: 'Extend Time＋',
+    55: 'Resistance-Board Restrict',
+    56: 'Skill Boost＋',
+    57: 'Enhance when HP is above 80%',
+    58: 'Enhance when HP is below 50%',
+    59: 'L-Shape Damage Reduction',
+    60: 'L-Shape Attack',
+    61: 'Super Enhanced Combo',
+}
 
 def make_defaultlist(fx, initial=[]):
     df = defaultlist(fx)
@@ -1102,7 +1166,7 @@ def suicide_random_nuke_convert(arguments):
         if c['hp_remaining'] == 0:
             c['skill_text'] += 'Reduce HP to 1; '
         else:
-            c['skill_text'] += 'Reduce HP by ' + fmt_mult(c['hp_remaining'] * 100) + '%; '
+            c['skill_text'] += 'Reduce HP by ' + fmt_mult((1 - c['hp_remaining']) * 100) + '%; '
         if c['minimum_multiplier'] != c['maximum_multiplier']:
             c['skill_text'] += 'Randomized ' + ATTRIBUTES[c['attribute']] + ' damage to ' + fmt_mass_atk(
                 c['mass_attack']) + '(' + fmt_mult(c['minimum_multiplier']) + '~' + fmt_mult(c['maximum_multiplier']) + 'x)'
