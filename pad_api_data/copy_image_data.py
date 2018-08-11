2c3c4ab673aa1317cc2c5f1ee1fa26a3dcc99e5b
nachoapps <nachoappsinc@gmail.com>
2018-08-10 21:35:17 -0400

    Change copy code to write high and low quality padguide images.

--- pad_api_data/copy_image_data.py
+++ pad_api_data/copy_image_data.py
@@ -4,6 +4,7 @@ Copies PAD images to the expected PadGuide locations.
 import argparse
 import os
 import shutil
+from PIL import Image
 
 from pad_etl.common import monster_id_mapping
 
@@ -21,11 +22,17 @@ def parse_args():
     return parser.parse_args()
 
 
-def do_copy(src_dir, src_file, dest_dir, dest_file):
+def do_copy(src_dir, src_file, dest_dir, dest_file, resize=None):
     src_path = os.path.join(src_dir, src_file)
     dest_path = os.path.join(dest_dir, dest_file)
-    if os.path.exists(src_path) and not os.path.exists(dest_file):
-        shutil.copy2(src_path, dest_path)
+    if os.path.exists(src_path) and not os.path.exists(dest_path):
+        if resize:
+            im = Image.open(src_path)
+            new_size = (im.size[0] * resize, im.size[1] * resize)
+            im.thumbnail(new_size, Image.ANTIALIAS)
+            im.save(dest_path)
+        else:
+            shutil.copy2(src_path, dest_path)
 
 
 def copy_images(args):
@@ -44,7 +51,10 @@ def copy_images(args):
         do_copy(jp_icon_input_dir, '{}.png'.format(jp_id),
                 output_dir, 'icon_{}.png'.format(monster_no_filled))
         do_copy(jp_portrait_input_dir, '{}.png'.format(jp_id),
-                output_dir, 'portrait_{}.png'.format(monster_no_filled))
+                output_dir, 'portrait_{}.png'.format(monster_no_filled),
+                resize=.5)
+        do_copy(jp_portrait_input_dir, '{}.png'.format(jp_id),
+                output_dir, 'texture_{}.png'.format(monster_no_filled))
 
     for na_id in monster_id_mapping.NA_VOLTRON_IDS:
         monster_no = monster_id_mapping.na_id_to_monster_no(na_id)
@@ -52,7 +62,10 @@ def copy_images(args):
         do_copy(na_icon_input_dir, '{}.png'.format(na_id),
                 output_dir, 'icon_{}.png'.format(monster_no_filled))
         do_copy(na_portrait_input_dir, '{}.png'.format(na_id),
-                output_dir, 'portrait_{}.png'.format(monster_no_filled))
+                output_dir, 'portrait_{}.png'.format(monster_no_filled),
+                resize=.5)
+        do_copy(na_portrait_input_dir, '{}.png'.format(na_id),
+                output_dir, 'texture_{}.png'.format(monster_no_filled))
 
 
 if __name__ == '__main__':
