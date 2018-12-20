121700592b8d3a03f39e5214a85d60415ec9060d
nachoapps <nachoappsinc@gmail.com>
2018-12-19 23:22:39 -0500

    Adding a script to download the hq image files

---
+++ image_pull/update_hq_image_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Updates the local cache of hq monster pics and uploads them to GCS.
+
+RUN_DIR=/home/tactical0retreat/rpad-cogs-utils/image_pull
+IMG_DIR=/home/tactical0retreat/image_data
+
+python3 ${RUN_DIR}/PADImageDownload.py --alt_input_dir=${IMG_DIR}/jp/full/raw_data --output_dir=${IMG_DIR}/hq_images
+gsutil -m rsync -r ${IMG_DIR}/hq_images gs://mirubot/padimages/hq_images
