ae8bc43d518d22333c5745854e7df5e63b098ea7
nachoapps <nachoappsinc@gmail.com>
2019-01-03 17:23:12 +0000

    adding some docs and helpers

--- pad_api_data/pad_etl/processor/wave.py
+++ pad_api_data/pad_etl/processor/wave.py
@@ -10,12 +10,13 @@ from .monster import SqlItem
 
 
 class WaveItem(SqlItem):
+    DROP_MONSTER_ID_GOLD = 9900
     def __init__(self, pull_id: int, entry_id: int, server: str, dungeon_id: int, floor_id: int, stage: int, slot: int, monster: wave_data.WaveMonster):
         self.server = server
         self.dungeon_id = dungeon_id
-        self.floor_id = floor_id
-        self.stage = stage
-        self.slot = slot
+        self.floor_id = floor_id # ID starts at 1 for lowest
+        self.stage = stage # 0-indexed
+        self.slot = slot # 0-indexed
 
         self.spawn_type = monster.spawn_type
         self.monster_id = monster.monster_id
@@ -28,6 +29,15 @@ class WaveItem(SqlItem):
 
         self.pull_id = pull_id
         self.entry_id = entry_id
+
+    def is_invade():
+        return self.spawn_type == 2
+
+    def get_drop(self):
+        return self.drop_monster_id if self.drop_monster_id > 0 and self.get_coins() == 0  else None
+
+    def get_coins(self):
+        return self.drop_monster_level if self.drop_monster_id == WaveItem.DROP_MONSTER_ID_GOLD else 0
 
     def _table(self):
         return 'wave_data'
