e7570632f3a7fb0c5ef9853442428726d170aadd
Rohil Thopu <rohilthopu@gmail.com>
2019-01-25 14:12:13 -0600

    Added item called required_floor, which happens to be the item after raw value for the required_dungeon, changed stat_modifiers to be split between dungeon and team stat modifiers

--- pad_api_data/pad_etl/common/dungeon_parse.py
+++ pad_api_data/pad_etl/common/dungeon_parse.py
@@ -4,9 +4,11 @@ from dungeon_maps import ENHANCED_TYPE_MAP, ENHANCED_ATTRIBUTE_MAP, TEAM_REQUIRE
 class Modifier:
     def __init__(self):
         self.required_dungeon = None
+        self.required_floor = None
         self.remaining_modifiers = []
         self.entry_requirement = ""
-        self.stat_modifiers = {}
+        self.team_stat_modifiers = {}
+        self.encounter_stat_modifiers = {}
         self.messages = []
         self.fixed_team = {}
         self.enhanced_type = ""
@@ -27,7 +29,9 @@ def get_modifiers(raw):
     dungeon_modifiers = Modifier()
 
     # This indicates the types of things you should build your team around, i.e. No Awoken Skills, Tricolor, No RCV, etc
-    dungeon_modifiers.messages.append(TEAM_REQUIREMENT_MAP[int(raw[7])])
+    team_requirement = TEAM_REQUIREMENT_MAP[int(raw[7])]
+    if team_requirement is not None:
+        dungeon_modifiers.messages.append(team_requirement)
 
     # This next loop runs through the elements from raw[8] until it hits a 0. The 0 indicates the end of the list
     # of drops for the floor, the following segments are the dungeon modifiers
@@ -46,12 +50,21 @@ def get_modifiers(raw):
 
     val = int(raw[pos])
 
-    if val == 5:
-        dungeon_modifiers.required_dungeon = int(raw[pos + 1])
+    if val == 1:
+        dungeon_modifiers.required_dungeon = int(raw[pos+1])
+        dungeon_modifiers.required_floor = int(raw[pos+2])
+
+    elif val == 5:
+        dungeon_modifiers.required_dungeon = int(raw[pos+1])
+        dungeon_modifiers.required_floor = int(raw[pos+2])
         return dungeon_modifiers
 
     elif val == 8:
-        dungeon_modifiers.score = raw[pos + 1]
+        dungeon_modifiers.score = int(raw[pos + 1])
+        return dungeon_modifiers
+
+    elif val == 9:
+        dungeon_modifiers.score = int(raw[pos + 3])
         return dungeon_modifiers
 
     elif val == 32:
@@ -60,11 +73,13 @@ def get_modifiers(raw):
 
     elif val == 33:
         dungeon_modifiers.required_dungeon = int(raw[pos + 1])
+        dungeon_modifiers.required_floor = int(raw[pos + 2])
         dungeon_modifiers.entry_requirement = ENTRY_REQUIREMENT_MAP[int(raw[pos + 3])](raw)
         return dungeon_modifiers
 
     elif val == 37:
         dungeon_modifiers.required_dungeon = int(raw[pos + 1])
+        dungeon_modifiers.required_floor = int(raw[pos + 2])
         dungeon_modifiers.entry_requirement = ENTRY_REQUIREMENT_MAP[int(raw[-2])](raw)
         return dungeon_modifiers
 
@@ -99,44 +114,47 @@ def get_modifiers(raw):
                 enhanced_type_raw = int(split_btype[0].split(':')[-1])
                 mods = split_btype[1:]
                 dungeon_modifiers.enhanced_type = ENHANCED_TYPE_MAP[enhanced_type_raw]
-                dungeon_modifiers.stat_modifiers['hp'] = float(mods[0]) / 10000
-                dungeon_modifiers.stat_modifiers['atk'] = float(mods[1]) / 10000
-                dungeon_modifiers.stat_modifiers['rcv'] = float(mods[2]) / 10000
+                dungeon_modifiers.team_stat_modifiers['hp'] = float(mods[0]) / 10000
+                dungeon_modifiers.team_stat_modifiers['atk'] = float(mods[1]) / 10000
+                dungeon_modifiers.team_stat_modifiers['rcv'] = float(mods[2]) / 10000
 
             elif m.startswith('battr'):
                 split_btype = m.split(';')
                 val = int(split_btype[0].split(':')[-1])
                 mods = split_btype[1:]
                 dungeon_modifiers.enhancedAttribute = ENHANCED_ATTRIBUTE_MAP[val]
-                dungeon_modifiers.stat_modifiers['hp'] = float(mods[0]) / 10000
-                dungeon_modifiers.stat_modifiers['atk'] = float(mods[1]) / 10000
-                dungeon_modifiers.stat_modifiers['rcv'] = float(mods[2]) / 10000
+                dungeon_modifiers.team_stat_modifiers['hp'] = float(mods[0]) / 10000
+                dungeon_modifiers.team_stat_modifiers['atk'] = float(mods[1]) / 10000
+                dungeon_modifiers.team_stat_modifiers['rcv'] = float(mods[2]) / 10000
 
             elif m.startswith('btype'):
                 hp_val = m.split(':')[-1]
-                dungeon_modifiers.stat_modifiers['fixed_hp'] = float(hp_val)
+                dungeon_modifiers.team_stat_modifiers['fixed_hp'] = float(hp_val)
             elif m.startswith('ndf'):
                 dungeon_modifiers.messages.append("No Skyfall Combos")
             elif m.startswith('hp'):
                 hp_val = m.split(':')[-1]
-                dungeon_modifiers.stat_modifiers['hp'] = float(hp_val) / 10000
+                dungeon_modifiers.encounter_stat_modifiers['hp'] = float(hp_val) / 10000
             elif m.startswith('atk'):
                 atk_val = m.split(':')[-1]
-                dungeon_modifiers.stat_modifiers['atk'] = float(atk_val) / 10000
+                dungeon_modifiers.encounter_stat_modifiers['atk'] = float(atk_val) / 10000
             elif m.startswith('df'):
                 df_val = m.split(':')[-1]
-                dungeon_modifiers.stat_modifiers['def'] = float(df_val) / 10000
+                dungeon_modifiers.encounter_stat_modifiers['def'] = float(df_val) / 10000
             else:
                 dungeon_modifiers.remaining_modifiers.append(m)
         return dungeon_modifiers
 
     elif val == 65:
         dungeon_modifiers.required_dungeon = int(raw[pos + 1])
+        dungeon_modifiers.required_floor = int(raw[pos + 2])
+
         dungeon_modifiers.remaining_modifiers = split_modifiers(raw, pos, 2)
         return dungeon_modifiers
 
     elif val == 69:
         dungeon_modifiers.required_dungeon = int(raw[pos + 1])
+        dungeon_modifiers.required_floor = int(raw[pos + 2])
         dungeon_modifiers.remaining_modifiers = split_modifiers(raw, pos, 4)
         return dungeon_modifiers
 
