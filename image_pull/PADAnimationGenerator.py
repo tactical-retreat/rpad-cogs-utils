19302d0b84545b01201f57406d35dddefd209ffd
nachoapps <nachoappsinc@gmail.com>
2019-02-23 23:09:27 -0500

    Adding support for generating gifs

--- image_pull/PADAnimationGenerator.py
+++ image_pull/PADAnimationGenerator.py
@@ -18,13 +18,25 @@ helpGroup = parser.add_argument_group("Help")
 helpGroup.add_argument("-h", "--help", action="help", help="Displays this help message and exits.")
 args = parser.parse_args()
 
+GENERATE_GIF_CMD = """
+ffmpeg -i {} \
+  -filter_complex 'scale=400:244:flags=lanczos,split [o1] [o2];[o1] palettegen [p]; [o2] fifo [o3];[o3] [p] paletteuse' \
+  -r 10 {}
+"""
 
-def generate_resized_image(source_file, dest_file):
-    ffmpg_cmd = 'ffmpeg -i {} -pix_fmt yuv420p -r 24 -c:v libx264 -filter:v "crop=640:390:0:60" {}'.format(
-        source_file, dest_file)
+OPTIMIZE_GIF_CMD = """
+gifsicle -O3 --lossy=80 {} -o {}
+"""
 
-    print('running', ffmpg_cmd)
-    os.system(ffmpg_cmd)
+RESIZE_VIDEO_CMD = """
+ffmpeg -i {} -pix_fmt yuv420p -r 24 -c:v libx264 -filter:v "crop=640:390:0:60" {}
+"""
+
+
+def run_cmd(cmd, source_file, dest_file):
+    final_cmd = cmd.format(source_file, dest_file).strip()
+    print('running', final_cmd)
+    os.system(final_cmd)
     print('done')
 
 
@@ -50,17 +62,30 @@ for file_name in sorted(os.listdir(raw_dir)):
         continue
 
     pad_id = file_name.rstrip('.isanimated').lstrip('mons_').lstrip('0')
-    final_image_name = '{}.mp4'.format(pad_id)
-    corrected_file_path = os.path.join(output_dir, final_image_name)
 
-    if os.path.exists(corrected_file_path):
-        print('skipping', corrected_file_path)
-        continue
+    video_name = '{}.mp4'.format(pad_id)
+    video_path = os.path.join(output_dir, video_name)
 
-    print('processing', corrected_file_path)
-    with tempfile.TemporaryDirectory() as temp_dir:
-        tmp_corrected_file_path = os.path.join(temp_dir, final_image_name)
-        process_animated(working_dir, pad_id, tmp_corrected_file_path)
-        generate_resized_image(tmp_corrected_file_path, corrected_file_path)
+    if os.path.exists(video_path):
+        print('skipping', video_path)
+    else:
+        print('processing', video_path)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            tmp_video_path = os.path.join(temp_dir, video_name)
+            process_animated(working_dir, pad_id, tmp_video_path)
+            run_cmd(RESIZE_VIDEO_CMD, tmp_video_path, video_path)
+
+    gif_name = '{}.gif'.format(pad_id)
+    gif_path = os.path.join(output_dir, gif_name)
+
+    if os.path.exists(gif_path):
+        print('skipping', gif_path)
+    else:
+        print('processing', gif_path)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            tmp_gif_path = os.path.join(temp_dir, gif_name)
+            run_cmd(GENERATE_GIF_CMD, video_path, tmp_gif_path)
+            run_cmd(OPTIMIZE_GIF_CMD, tmp_gif_path, gif_path)
+
 
 print('done')
