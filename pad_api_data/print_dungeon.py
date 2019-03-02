e0230065e1d84e19c0afc472fae89a36ecfec976
nachoapps <nachoappsinc@gmail.com>
2019-03-02 18:00:39 -0500

    Minor cleanups

--- pad_api_data/print_dungeon.py
+++ pad_api_data/print_dungeon.py
@@ -6,6 +6,7 @@ import os
 from collections import defaultdict
 
 from pad_etl.data import database
+from pad_etl.data import wave
 from pad_etl.processor import enemy_skillset_processor
 
 fail_logger = logging.getLogger('processor_failures')
@@ -36,14 +37,10 @@ dungeon_id = int(args.dungeon_id) if args.dungeon_id else None
 na_database = database.Database('na', args.raw_input_dir)
 na_database.load_database(skip_skills=True, skip_bonus=True, skip_extra=True)
 
-
 dungeon_id_to_wavedata = defaultdict(list)
-wave_summary_file = os.path.join(args.processed_input_dir, 'wave_summary.csv')
-with open(wave_summary_file) as f:
-    csvreader = csv.reader(f, delimiter=',', quotechar='"')
-    next(csvreader)
-    for row in csvreader:
-        dungeon_id_to_wavedata[int(row[0])].append(row)
+wave_summary_data = wave.load_wave_summary(args.processed_input_dir)
+for wave in wave_summary_data:
+    dungeon_id_to_wavedata[wave.dungeon_id].append(wave)
 
 if not dungeon_id:
     print('no dungeon_id param specified; dumping all dungeons')
@@ -69,15 +66,14 @@ print(dungeon.clean_name, '-', floor.clean_name)
 
 stage_to_monsters = defaultdict(list)
 for wave in waves:
-    floor_id = int(wave[1])
-    stage = int(wave[2])
-    spawn_type = int(wave[3])
-    monster_id = int(wave[4])
-    monster_level = int(wave[5])
+    floor_id = wave.floor_id
+    stage = wave.stage
+    monster_id = wave.monster_id
+    monster_level = wave.monster_level
 
     if floor_id != floor.floor_number:
         continue
-    if spawn_type == 2:
+    if wave.spawn_type == 2:
         continue
 
     stage_to_monsters[stage].append((monster_id, monster_level))
