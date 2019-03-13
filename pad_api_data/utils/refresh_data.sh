ceeac5da385d2bfd1a1dfa0572a0f377745a0662
nachoapps <nachoappsinc@gmail.com>
2019-03-12 22:10:32 -0400

    add wave_summary to download list

--- pad_api_data/utils/refresh_data.sh
+++ pad_api_data/utils/refresh_data.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
 mkdir pad_data
 mkdir pad_data/raw
+mkdir pad_data/processed
 gsutil -m rsync -r -c gs://mirubot-data/paddata/raw pad_data/raw
+gsutil -m rsync -x "(?!^wave_summary.csv$)" gs://mirubot-data/paddata/processed pad_data/processed
