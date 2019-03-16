1a76bd338b2a038da373f4565133d3003c8dbc5e
xiaxinti <michelle.xia@mail.utoronto.ca>
2019-03-16 00:46:34 -0400

    trim all loops, single or multi

--- pad_api_data/pad_etl/processor/enemy_skillset_processor.py
+++ pad_api_data/pad_etl/processor/enemy_skillset_processor.py
@@ -433,7 +433,7 @@ def convert(enemy_behavior: List, level: int):
         ctx.turn_event()
 
     # Loop over every turn
-    behavior_loops = []
+    behavior_loop = None
     for i_idx, check_data in enumerate(turn_data):
         # Loop over every following turn. If the outer turn matches an inner turn moveset,
         # we found a loop.
@@ -468,26 +468,17 @@ def convert(enemy_behavior: List, level: int):
                 possible_loops.remove((check_start, check_end))
 
         if len(possible_loops) > 0:
-            behavior_loops.append(possible_loops[0])
+            behavior_loop = possible_loops[0]
+            break
 
     # Process loops
-    looped_behavior = []
-    if len(behavior_loops) > 0:
-        loop_start, loop_end = behavior_loops[0]
+    looped_behavior = []  # keep track of behaviour added to loops
+    if behavior_loop is not None:
+        loop_start, loop_end = behavior_loop
         loop_size = loop_end - loop_start
-        if loop_size == 1:
-            # Since this isn't a multi-turn looping moveset, try to trim the earlier turns.
-            looping_behavior = turn_data[loop_start]
-            for idx in range(loop_start):
-                check_turn_data = turn_data[idx]
-                for hp, hp_behavior in looping_behavior.items():
-                    if check_turn_data.get(hp, None) == hp_behavior:
-                        check_turn_data.pop(hp)
 
-                for hp, hp_behavior in check_turn_data.items():
-                    skillset.timed_skill_groups.append(TimedSkillGroup(idx + 1, hp, hp_behavior))
-                    looped_behavior.append((hp, hp_behavior))
-        else:
+        # Handle a multi-turn loop
+        if loop_size > 1:
             # exclude any behavior present on all turns of the loop
             common_behaviors = [hp_b for hp_b in turn_data[loop_start].items()]
             for idx in range(loop_start + 1, loop_end):
@@ -500,6 +491,22 @@ def convert(enemy_behavior: List, level: int):
                         skillset.repeating_skill_groups.append(
                             TimedSkillGroup(idx + 1, hp, hp_behavior))
                         looped_behavior.append((hp, hp_behavior))
+
+        # Trim turns before the loop
+        looping_behavior = turn_data[loop_start]
+        for idx in range(loop_start):
+            check_turn_data = turn_data[idx]
+            for hp, hp_behavior in looped_behavior:
+                if check_turn_data.get(hp, None) == hp_behavior:
+                    check_turn_data.pop(hp)
+            for hp, hp_behavior in looping_behavior.items():
+                if check_turn_data.get(hp, None) == hp_behavior:
+                    check_turn_data.pop(hp)
+
+            for hp, hp_behavior in check_turn_data.items():
+                skillset.timed_skill_groups.append(TimedSkillGroup(idx + 1, hp, hp_behavior))
+                # looped_behavior.append((hp, hp_behavior))
+
 
     # Simulate enemies being defeated
     if has_enemy_remaining_branch:
