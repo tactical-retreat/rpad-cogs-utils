508b629344cf3982573b97f846c8f90120d60e0d
nachoapps <nachoappsinc@gmail.com>
2019-05-21 10:22:51 -0400

    Adding simple imports for commonly used classes

--- pad_api_data/pad_etl/data/__init__.py
+++ pad_api_data/pad_etl/data/__init__.py
@@ -4,3 +4,16 @@ data structures.
 
 It should only depend on items from the common package.
 """
+
+from . import bonus, card, dungeon, skill, exchange, enemy_skill
+
+Bonus = bonus.Bonus
+BookCard = card.BookCard
+Curve = card.Curve
+Enemy = card.Enemy
+EnemySkillRef = card.EnemySkillRef
+Dungeon = dungeon.Dungeon
+DungeonFloor = dungeon.DungeonFloor
+MonsterSkill = skill.MonsterSkill
+Exchange = exchange.Exchange
+EnemySkill = enemy_skill.EnemySkill
