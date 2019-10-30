75a090889383151a85ce33e789afa74a01f97ba3
nachoapps <nachoappsinc@gmail.com>
2019-10-29 22:11:19 -0400

    Deleting junk

--- pad_api_data/utils/refresh_data.bat
+++
@@ -1,5 +0,0 @@
-mkdir pad_data
-mkdir pad_data\raw
-mkdir pad_data\processed
-call gsutil -m rsync -r -c gs://mirubot-data/paddata/raw pad_data\raw
-call gsutil -m rsync -x "(?!^wave_summary.csv$)" gs://mirubot-data/paddata/processed pad_data\processed
