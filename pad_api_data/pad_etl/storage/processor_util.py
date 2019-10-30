b6cef1f0c0c46a6fd89d25423f1b9c2ff5120176
nachoapps <nachoappsinc@gmail.com>
2019-10-29 22:25:23 -0400

    deleting junk

--- pad_api_data/pad_etl/storage/processor_util.py
+++
@@ -1,7 +0,0 @@
-def normalize_pgserver(server: str):
-    server = server.lower()
-    if server == 'na':
-        server = 'us'
-    if server not in ('us', 'jp'):
-        raise ValueError('unexpected server:', server)
-    return server.upper()
