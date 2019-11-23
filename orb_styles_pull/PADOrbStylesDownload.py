16e0b3bf7db2724f4b0357d1519bae9c14104378
nachoapps <nachoappsinc@gmail.com>
2019-11-23 00:16:05 -0500

    Extract orbs from orb skins

--- orb_styles_pull/PADOrbStylesDownload.py
+++ orb_styles_pull/PADOrbStylesDownload.py
@@ -1,13 +1,11 @@
 import argparse
-import json
 import os
-import re
+import shutil
 import sys
 import urllib.request
 
-from collections import defaultdict
-
 import padtools
+from PIL import Image
 
 parser = argparse.ArgumentParser(
     description="Downloads P&D Orb Styles (alternate skins)", add_help=False)
@@ -18,6 +16,7 @@ inputGroup.add_argument("--data_dir", required=True, help="Path to processed pad
 
 outputGroup = parser.add_argument_group("Output")
 outputGroup.add_argument("--output_dir", help="Path to a folder where output should be saved")
+outputGroup.add_argument("--final_dir", help="Path to a folder where output should be saved")
 
 helpGroup = parser.add_argument_group("Help")
 helpGroup.add_argument("-h", "--help", action="help", help="Displays this help message and exits.")
@@ -31,8 +30,9 @@ if args.server == 'na':
 elif args.server == 'jp':
     extras = padtools.regions.japan.server.extras
 
-
 output_dir = args.output_dir
+final_dir = args.final_dir
+
 
 def download_file(url, file_path):
     response_object = urllib.request.urlopen(url)
@@ -41,17 +41,21 @@ def download_file(url, file_path):
         with open(file_path, "wb") as f:
             f.write(file_data)
 
+
 print('Found', len(extras), 'extras total')
 
 raw_dir = os.path.join(output_dir, 'raw')
 extract_dir = os.path.join(output_dir, 'extract')
 os.makedirs(raw_dir, exist_ok=True)
 os.makedirs(extract_dir, exist_ok=True)
+os.makedirs(final_dir, exist_ok=True)
 
 raw_dir = os.path.join(raw_dir, server)
 extract_dir = os.path.join(extract_dir, server)
+final_dir = os.path.join(final_dir, server)
 os.makedirs(raw_dir, exist_ok=True)
 os.makedirs(extract_dir, exist_ok=True)
+os.makedirs(final_dir, exist_ok=True)
 
 python_exec = sys.executable
 cur_file_path = os.path.dirname(os.path.realpath(__file__))
@@ -65,7 +69,7 @@ for extra in extras:
         print('skipping', raw_file_name)
         continue
 
-    raw_file_path = os.path.join(raw_dir, raw_file_name)    
+    raw_file_path = os.path.join(raw_dir, raw_file_name)
     if os.path.exists(raw_file_path) and not should_always_process:
         print('file exists', raw_file_path)
     else:
@@ -84,4 +88,38 @@ for extra in extras:
             tool=tool_path,
             input=raw_file_path,
             output=extract_dir))
+
+    final_file_name = extract_file_name.lower().replace('BLOCK', '')
+    final_file_path = os.path.join(final_dir, final_file_name)
+
+    if os.path.exists(final_file_path) and not should_always_process:
+        print('skipping existing file', final_file_path)
+    else:
+        print('copying', extract_file_path, 'to', final_file_path)
+        shutil.copy2(extract_file_path, final_file_path)
+
+        img = Image.open(final_file_path)
+        orb_width, spacer = 100, 4
+
+        x, y = 0, 0
+        orb_path = final_file_path.replace('.png', '_00.png')
+        img.crop(box=(x, y, x + orb_width, y + orb_width)).save(orb_path)
+
+        x += orb_width + spacer
+        orb_path = final_file_path.replace('.png', '_01.png')
+        img.crop(box=(x, y, x + orb_width, y + orb_width)).save(orb_path)
+
+        x += orb_width + spacer
+        orb_path = final_file_path.replace('.png', '_02.png')
+        img.crop(box=(x, y, x + orb_width, y + orb_width)).save(orb_path)
+
+        x += orb_width + spacer
+        orb_path = final_file_path.replace('.png', '_03.png')
+        img.crop(box=(x, y, x + orb_width, y + orb_width)).save(orb_path)
+
+        y += orb_width + spacer
+        x = 0
+        orb_path = final_file_path.replace('.png', '_04.png')
+        img.crop(box=(x, y, x + orb_width, y + orb_width)).save(orb_path)
+
 print('done')
--- orb_styles_pull/update_orb_styles_files.sh
+++ orb_styles_pull/update_orb_styles_files.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #
-# Updates the local cache of voice files, fixes them, and uploads them to gcs.
+# Updates the local cache of orb skin files.
 
 RUN_DIR=/home/tactical0retreat/rpad-cogs-utils/orb_styles_pull
 OUTPUT_DIR=/home/tactical0retreat/pad_data/orb_styles
 DATA_DIR=/home/tactical0retreat/pad_data/processed
+FINAL_DIR=/home/tactical0retreat/dadguide/data/media/orb_skins
 
 # Pull raws
 python3 ${RUN_DIR}/PADOrbStylesDownload.py --output_dir=${OUTPUT_DIR} --data_dir=${DATA_DIR} --server=na
